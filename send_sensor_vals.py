9a2a8072ca90dd77088b25e8f31ba899df873bae
NickTVA <Nicholas.W.Talbot@gmail.com>
2025-03-16 20:02:05 -0400

    Added memory leak demo

--- send_sensor_vals.py
+++ send_sensor_vals.py
@@ -10,13 +10,38 @@ import network
 import rp2
 import requests
 import gc
+import os
 from NR_metric import NRMetricElement, Attributes, Metric
 from environment import HOSTNAME, APPNAME, metrics_url, logs_url, WIFI_REGION_CODE
 from credentials import SSID, PASSCODE, NR_API_KEY
 
+
+RED_PIN_NUMBER = 21
+YELLOW_PIN_NUMBER = 20
+GREEN_PIN_NUMBER = 19
+
+green_led = Pin(GREEN_PIN_NUMBER, Pin.OUT)
+red_led = Pin(RED_PIN_NUMBER, Pin.OUT)
+yellow_led = Pin(YELLOW_PIN_NUMBER, Pin.OUT)
+
+sw3 =  Pin(15, Pin.IN, Pin.PULL_UP)
+
+
 #you may notice garbage collection called manually as the RP2350 this was tested on has limited memory
 #calling manually when large data structures such as HTTP requests / responses go out of scope reduces fragmentation
 gc.enable()
+
+#List for memory leak demo:
+memory_leak_list = []
+memory_leak_mode = False
+
+#holding down on sw3 during startup will send demo into memory leak mode
+print("sw3 value", sw3.value())
+if(sw3.value() == 0) :
+    memory_leak_mode = True
+    yellow_led.on()
+    red_led.on()
+
 
 #depending on hardware and micropython libraries time may be UTC or local
 #the NR API requires UTC or it will give a 202 and then throw the data away
@@ -99,6 +124,55 @@ if configError :
 #   for device in devices:
 #     print("I2C addr: ", hex(device))
 
+def report_system_metrics():
+    
+    try:
+        alloc_mem = float(gc.mem_alloc())
+        free_mem = float(gc.mem_free())
+        
+        vfs_statistics = os.statvfs("/")
+        vfs_size = vfs_statistics[1] * vfs_statistics[2]
+        vfs_free = vfs_statistics[0] * vfs_statistics[3]
+        vfs_allocated = vfs_size - vfs_free
+        
+        print("Allocated memory: ", alloc_mem)
+        print("Free memory: ", free_mem)
+        print("VFS_Size: ", vfs_size)
+        print("Allocated vfs: ", vfs_allocated)
+        print("Free vfs: ", vfs_free)
+        
+        unix_timestamp = time.time()
+        unix_timestamp += (UTC_OFFSET_HOURS * 3600)
+        
+        alloc_mem_metric = Metric("ram_allocated", "gauge", alloc_mem, unix_timestamp, attributes)
+        free_mem_metric = Metric("ram_free", "gauge", free_mem, unix_timestamp, attributes)
+        
+        size_vfs_metric = Metric("vfs_size_bytes", "gauge", float(vfs_size), unix_timestamp, attributes)
+        alloc_vfs_metric = Metric("vfs_allocated_bytes", "gauge", float(vfs_allocated), unix_timestamp, attributes)
+        free_vfs_metric = Metric("vfs_free_bytes", "gauge", float(vfs_free), unix_timestamp, attributes)
+        
+        sys_metric_elements = NRMetricElement([alloc_mem_metric, free_mem_metric, size_vfs_metric, alloc_vfs_metric, free_vfs_metric])
+
+        #New Relic metrics to be in an root array
+        send_bytes_string = json.dumps([ sys_metric_elements.to_dict() ])
+#        print(send_bytes_string)
+        send_bytes = send_bytes_string.encode()
+        header_data = {"Content-Type": "application/json", "Api-Key": NR_API_KEY}
+        r = requests.post(metrics_url, data=send_bytes, headers=header_data, timeout=20)
+        http_status_code = r.status_code
+        print(r.status_code)
+        r.close()
+        gc.collect()
+        if memory_leak_mode :
+            memory_leak_list.append(send_bytes)
+            memory_leak_list.append("Current time: " + str(unix_timestamp))
+
+        return http_status_code
+    
+    except Exception as e:
+        print("Error reporting system metrics", e)
+        return -1
+
 
 time.sleep(1)
 #connect WIFI
@@ -107,13 +181,17 @@ wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
 wlan.connect(SSID, PASSCODE)
 
+wlan_connection_attempts = 0
 while not wlan.isconnected() or wlan.status() < 0 :
+    wlan_connection_attempts += 1
     print("Connecting to WIFI:")
     display = ssd1306.SSD1306_I2C(128, 32, i2c)
     display.text("Connecting to WIFI",0,0)
     display.text(SSID, 0, 15)
     display.show()    
     time.sleep(2)
+    if wlan_connection_attempts > 30 :
+        reset()
 
 print(wlan.ifconfig())
 
@@ -123,6 +201,8 @@ print(wlan.ifconfig())
 
 #set microcontroller time from NTP
 ntptime.settime()
+
+report_system_metrics()
 
 # print(time.time())
 # print(time.time() + (UTC_OFFSET_HOURS * 3600))
@@ -134,11 +214,15 @@ init_timestamp += (UTC_OFFSET_HOURS * 3600)
 
 display.text(str(init_timestamp), 0, 15)
 display.show()
-time.sleep(9)
+time.sleep(5)
 
 
 #read sensors, display on oled, and send to NR
 def run():
+    
+    #    if (runs % 5) == 0:
+    report_system_metrics()
+    
     global display
     bme = BME280.BME280(i2c=i2c)
     light_sensor = BH1750(bus=i2c, addr=0x23)
@@ -164,7 +248,7 @@ def run():
     #toss out first sensor reads, as they can be erroneous
     if runs == 0 :
         return
-    
+        
     #send all env data to NR
     http_status_code = send_metric_to_nr(hum, lux, pres, tempF, unix_timestamp)
     
@@ -197,6 +281,9 @@ def send_metric_to_nr(hum, lux, pres, tempF, unix_timestamp):
     print(r.status_code)
     r.close()
     gc.collect()
+    if memory_leak_mode :
+        memory_leak_list.append(elements)
+        memory_leak_list.append(send_bytes)
     return http_status_code
 
 #send log line to NR directly
@@ -225,10 +312,24 @@ def send_log_to_nr(message, unix_timestamp):
 log_timestamp = time.time()
 log_timestamp += (UTC_OFFSET_HOURS * 3600)
 send_log_to_nr("RP2350 startup", log_timestamp)
+if memory_leak_mode :
+    send_log_to_nr("Running memory leak demo...", log_timestamp)
+    print("Starting memory leak demo")
+
+log_timestamp = time.time()
+log_timestamp += (UTC_OFFSET_HOURS * 3600)
+send_log_to_nr(wlan.ifconfig(), log_timestamp)
+
+
+
+        
+    
 
 
 while True:
     try:
+        if((runs > 700) & memory_leak_mode) :
+            reset()
         gc.collect()
         print("\nRun: {:d}".format(runs))
         run()
